e09f46d3e2029e1147411067a8b0258ae1b5fff9
Gourob Saha <gourobsaha009@gmail.com>
2022-09-03 15:25:37 +0600

    Sort by View Added & Updated

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // console.log('Hello');
+//Loading News Category API
 const loadNewsCategory = async (data) => {
     try {
         const url = `https://openapi.programming-hero.com/api/news/categories`;
@@ -11,6 +12,7 @@ const loadNewsCategory = async (data) => {
     }
 }
 
+//Displaying News Category in News Page
 const displayCategoryName = (categories) => {
     // console.log(categories)
     const categoryContainer = document.getElementById('category-container');
@@ -26,7 +28,7 @@ const displayCategoryName = (categories) => {
 
 }
 
-// 
+// Function for Spinner
 const toggleSpinner = isLoading => {
     const loadingSection = document.getElementById('loader');
     if (isLoading) {
@@ -37,6 +39,7 @@ const toggleSpinner = isLoading => {
     }
 }
 
+//Loading News Category ID from API
 const loadNews = async (category_id) => {
     try {
         const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
@@ -49,12 +52,15 @@ const loadNews = async (category_id) => {
     }
 }
 
+//Displaying News in News Page
 const displayNews = (categoryId) => {
     // console.log(categoryId.data);
     const newsLength = (categoryId.data).length;
     const itemNumber = document.getElementById('number-of-items');
     itemNumber.textContent = '';
     const itemsDiv = document.createElement('div');
+
+    //Displaying Number of News
     itemsDiv.classList.add('text-center')
     if (newsLength <= 0) {
         itemsDiv.innerHTML = `
@@ -69,10 +75,16 @@ const displayNews = (categoryId) => {
     itemNumber.appendChild(itemsDiv);
 
     const newsItems = categoryId.data;
+    // console.log(categoryId.data);
+    //Sorting the News by Most Views
+    newsItems.sort((a, b) => {
+        return b.total_view - a.total_view;
+    });
     const newsItemsContainer = document.getElementById('news-items');
     newsItemsContainer.textContent = '';
     newsItems.forEach(newsItem => {
         // console.log(newsItem);
+        //Displaying Sorted News
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('mb-5')
         newsDiv.innerHTML = `
@@ -85,15 +97,15 @@ const displayNews = (categoryId) => {
                         <div class="flex items-center">
                             <img class="w-16 rounded-full mr-5" src=${newsItem.author.img}" alt="Album">
                             <div class="">
-                            <p class="font-bold">${newsItem.author.name ? newsItem.author.name : "Anonymous"}</p>
-                            <p>${newsItem.author.published_date ? newsItem.author.published_date : "No Date"}</p>
+                                <p class="font-bold">${newsItem.author.name ? newsItem.author.name : "Anonymous"}</p>
+                                <p>${newsItem.author.published_date ? newsItem.author.published_date : "No Date"}</p>
                             </div>
                         </div>
                         <div class="flex items-center">
-                            <i class="fa-solid fa-eye mr-2"></i><p class="font-semibold"> Views: ${newsItem.total_view ? newsItem.total_view : "Hidden"}</p>
+                            <i class="fa-solid fa-eye mr-2"></i><p class="font-semibold"> Views: ${newsItem.total_view ? newsItem.total_view : "No Views"}</p>
                         </div>
                         <div class="flex items-center">
-                            <i class="fa-solid fa-star mr-2"></i><p class="font-semibold"> Rating: ${newsItem.rating.number ? newsItem.rating.number : "Hidden"}</p>
+                            <i class="fa-solid fa-star mr-2"></i><p class="font-semibold"> Rating: ${newsItem.rating.number ? newsItem.rating.number : "None"}</p>
                         </div>
                         <label for="news-modal" onclick="loadNewsDetails('${newsItem._id}')" class="btn btn-outline">View More <i class="ml-2 fa-solid fa-circle-chevron-right"></i></label>
                 </div>
@@ -105,6 +117,7 @@ const displayNews = (categoryId) => {
 
 }
 
+//Loading Detailed News form API
 const loadNewsDetails = async (news_id) => {
     try {
         const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
@@ -117,13 +130,32 @@ const loadNewsDetails = async (news_id) => {
     }
 }
 
+//Displaying Detailed News Into Modal
 const modalNews = (newsDetails) => {
-    console.log(newsDetails);
+    // console.log(newsDetails);
     const newsDetailsModal = document.getElementById('modal-news-details');
     newsDetailsModal.innerHTML = `
         <img class="mb-5" src=${newsDetails.image_url}" alt="Album">
         <h2 class="text-xl font-semibold">${newsDetails.title}</h2>
         <p class="text-stone-500">${(newsDetails.details)}...</p>
+        <div class="flex items-center my-5">
+            <img class="w-16 rounded-full mr-5" src=${newsDetails.author.img}" alt="Album">
+            <div class="">
+                <p class="font-bold">${newsDetails.author.name ? newsDetails.author.name : "Anonymous"}</p>
+                <p>${newsDetails.author.published_date ? newsDetails.author.published_date : "No Date"}</p>
+            </div>
+        </div>
+        <div class="flex justify-between mt-2">
+            <div class="flex items-center">
+                <i class="fa-solid fa-eye mr-2"></i><p class="font-semibold"> Views: ${newsDetails.total_view ? newsDetails.total_view : "No Views"}</p>
+            </div>
+            <div class="flex items-center">
+                <i class="fa-solid fa-star mr-2"></i><p class="font-semibold"> Rating: ${newsDetails.rating.number ? newsDetails.rating.number : "None"}</p>
+            </div>
+            <div class="flex items-center">
+                <i class="fa-regular fa-star mr-2"></i><p class="font-semibold"> Badge: ${newsDetails.rating.badge ? newsDetails.rating.badge : "None"}</p>
+            </div>
+        </div>
     `;
 
 }
